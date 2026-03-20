771755f9e4941b8525effd37cdab46efdc19c468
IgorSoftex <igor.softex@ukr.net>
2026-03-20 11:51:29 +0200

    Edited file admin.py

--- backend/apps/comments/admin.py
+++ backend/apps/comments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Comment # импортируем нашу модель
 
-# Register your models here.
+# декоратор, который регистрирует модель Comment в админке
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'email', 'parent', 'created_at')
+    list_filter = ('created_at',) # фильтр справа в админке, позволяет фильтровать по дате
+    search_fields = ('user_name', 'email', 'text') # поле поиска вверху, ищет по user_name, email и text
+    ordering = ('-created_at',) # сортировка в админке от более новых до старых
